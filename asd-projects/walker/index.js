c777e61001ec27ac0f3636258ff1f1e07b638e79
PeytonStrahan <PGS4262@ppsbstudent.org>
2023-03-10 01:09:30 +0000

    Added index.html

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,5 +1,14 @@
 /* global $, sessionStorage */
 
+var amIt = getRandomBetween(0, 2);
+
+var walkerOneScore = 0;
+var walkerTwoScore = 0;
+
+function getRandomBetween(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
   
 function runProgram(){
@@ -17,17 +26,43 @@ function runProgram(){
     "DOWN": 83,
   }
 
+  var KEY2 = {
+    "LEFT":37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40,
+  }
+
+  // var walker = jQuery('.walker');	// reference to the HTML .walker element
+	// var walker2 = jQuery('.walker2');	// reference to the HTML .walker2 element
+
+  document.getElementById("walker").innerHTML = walkerOneScore;
+  document.getElementById("walker2").innerHTML = walkerTwoScore;
+
+  walker.style.backgroundColor = 'cyan';
+  walker2.style.backgroundColor = 'greenyellow';
+
+	// var boardWidth = board.width();	// the maximum X-Coordinate of the screen
+	// var boardHeight = board.height();
+  
   // Game Item Objects
   var positionX = 0; // the x-coordinate location for the walker
   var positionY = 0; // the y-coordinate location for the walker
   var speedX = 0; // the speed for the walker along the x-axis
   var speedY = 0; // the speed for the walker along the y-axis
 
+  var positionX2 = 390; // the x-coordinate location for the second walker
+  var positionY2 = 390; // the y-coordinate location for the second walker
+  var speedX2 = 0; // the speed for the second walker along the x-axis
+  var speedY2 = 0; // the speed for the second walker along the y-axis
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);                           // change 'eventType' to the type of event you want to handle...again
+
+  $(document).on('keydown', handleKeyDown2);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp2);                           // change 'eventType' to the type of event you want to handle...again
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -40,6 +75,7 @@ function runProgram(){
   function newFrame() {
     repositionGameItem();
     redrawGameItem();
+    checkForDeath();
   }
   
   /* 
@@ -83,6 +119,51 @@ function runProgram(){
     }
   }
 
+  function handleKeyDown2(event) {
+    if (event.which === KEY2.LEFT) {
+      console.log("left pressed");
+      speedX2 = -5;
+    }
+    else if (event.which === KEY2.UP) {
+      console.log("up pressed");
+      speedY2 = -5;
+    }
+    else if (event.which === KEY2.RIGHT) {
+      console.log("right pressed");
+      speedX2 = 5;
+    }
+    else if (event.which === KEY2.DOWN) {
+      console.log("down pressed");
+      speedY2 = 5;
+    }
+  }
+
+  function handleKeyUp2(event) {
+    if (event.which === KEY2.LEFT) {
+      console.log("left unpressed");
+      speedX2 = 0;
+    }
+    else if (event.which === KEY2.UP) {
+      console.log("up unpressed");
+      speedY2 = 0;
+    }
+    else if (event.which === KEY2.RIGHT) {
+      console.log("right unpressed");
+      speedX2 = 0;
+    }
+    else if (event.which === KEY2.DOWN) {
+      console.log("down unpressed");
+      speedY2 = 0;
+    }
+  }
+
+  if (amIt === 0) {
+    setTimeout(walkerOneIsIt, 5000);
+  }
+  else if (amIt === 1) {
+    setTimeout(walkerTwoIsIt, 5000);
+  }
+
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -97,13 +178,82 @@ function runProgram(){
   }
 
   function repositionGameItem() {
+    if (speedX === -5 && positionX <= 0) {
+      speedX = 0;
+    }
+    else if (speedX === 5 && positionX >= 390) {
+      speedX = 0;
+    }
+    if (speedY === -5 && positionY <= 0) {
+      speedY = 0;
+    }
+    else if (speedY === 5 && positionY >= 390) {
+      speedY = 0;
+    }
+
+    if (speedX2 === -5 && positionX2 <= 0) {
+      speedX2 = 0;
+    }
+    else if (speedX2 === 5 && positionX2 >= 390) {
+      speedX2 = 0;
+    }
+    if (speedY2 === -5 && positionY2 <= 0) {
+      speedY2 = 0;
+    }
+    else if (speedY2 === 5 && positionY2 >= 390) {
+      speedY2 = 0;
+    }
     positionX += speedX; // update the position of the walker along the x-axis
     positionY += speedY; // update the position of the walker along the y-axis
+    positionX2 += speedX2; // update the position of the second walker along the x-axis
+    positionY2 += speedY2; // update the position of the second walker along the y-axis
   }
 
   function redrawGameItem() {
     $("#walker").css("left", positionX);    // draw the walker in the new location, positionX pixels away from the "left"
     $("#walker").css("top", positionY);    // draw the walker in the new location, positionY pixels away from the "top"
+    $("#walker2").css("left", positionX2);    // draw the second walker in the new location, positionX pixels away from the "left"
+    $("#walker2").css("top", positionY2);    // draw the second walker in the new location, positionY pixels away from the "top"
   }
+
+  function walkerOneIsIt() {
+    walker.style.backgroundColor = 'red';
+  }
+
+  function walkerTwoIsIt() {
+    walker2.style.backgroundColor = 'red';
+  }
+
+  function checkForDeath() {
+    var a = (positionX + 25) - (positionX2 + 25);
+    var b = (positionY - 25) - (positionY2 - 25);
+
+    var c = Math.sqrt( a*a + b*b );
+    if (walker.style.backgroundColor === 'red') {
+      if (c <= 50) {
+        walkerOneScore ++;
+        amIt = 1;
+        endGame();
+        runProgram();
+      }
+    }
+    if (walker2.style.backgroundColor === 'red') {
+      if (c <= 50) {
+        walkerTwoScore ++;
+        amIt = 0;
+        endGame();
+        runProgram();
+      }
+    }
+
+  }
+
+  /* function changeWalkerOneText(newText) {
+    walker.text = newText;
+  }
+
+  function changeWalkerTwoText(newText) {
+    walker2.text = newText;
+  } */
   
 }
